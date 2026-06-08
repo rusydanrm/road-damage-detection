83a311f64ea4fecbda3ab351eb1f4c5d5993bace
Rafi Muhammad Rusydan <rafimrusydan@gmail.com>
2026-06-09 05:32:37 +0700

    first

--- backend/app.py
+++ backend/app.py
@@ -152,4 +152,5 @@ def too_large(e):
 if __name__ == "__main__":
     # Jalankan Flask server
     # Debug=True untuk development, matikan saat production
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
